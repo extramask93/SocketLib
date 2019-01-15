6c5a3557310239e55ff68b22c113c29b7fa39ba7
Damian Jozwiak <jozwiak.damian02@gmail.com>
2019-01-15 14:29:13 +0100

    added support for linux

--- SocketTCP.cpp
+++ SocketTCP.cpp
@@ -1,17 +1,30 @@
 #include "SocketException.h"
 #include "SocketTCP.h"
 #include <string>
+#ifdef _MSC_VER
+#define CLOSE(sock) closesocket(sock)
 #include <Ws2tcpip.h>
+#else
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h>
+#define CLOSE(sock) close(sock)
+#endif
+
 
 #define CONNECT_REQUIRED {if(sock == INVALID_SOCKET) {throw SocketException{"Socket not connected, please connect before use."};}}
 
 SocketTCP::SocketTCP()
 {
-	const auto err = WSAStartup(MAKEWORD(2, 2), &this->wsa);
-	if(err != 0)
-	{
-		throw SocketException{"WSAStartup failed with code: "+ std::to_string(err)};
-	}
+#ifdef _MSC_VER
+const auto err = WSAStartup(MAKEWORD(2, 2), &this->wsa);
+
+if(err != 0)
+{
+    throw SocketException{"WSAStartup failed with code: "+ std::to_string(err)};
+}
+#endif
 }
 
 SocketTCP::State SocketTCP::TCPConnect(const std::string& remoteAddress,
@@ -45,16 +58,29 @@ SocketTCP::State SocketTCP::TCPConnect(const std::string& remoteAddress,
 			break;
 		}
 
-		closesocket(sock);
+        CLOSE(sock);
 	}
 
 	if (resultPointer == nullptr)
 	{
 		throw SocketException("Cannot establish connection to the server, error code: "
-			+ std::to_string(WSAGetLastError()));
+            /*+ std::to_string(WSAGetLastError())*/);
 	}
 	::freeaddrinfo(result);
-	return State::Done;
+    return State::Done;
+}
+
+size_t SocketTCP::TCPReveiveUtilClosed(std::string &buff)
+{
+    CONNECT_REQUIRED;
+    char buffer;
+    size_t bytesRead = 0;
+    buff.clear();
+    while (TCPReceiveChar(&buffer) == State::Done) {
+        buff.push_back(buffer);
+        ++bytesRead;
+    }
+    return bytesRead;
 }
 
 SocketTCP::State SocketTCP::TCPSend(const char *data, int size) const
@@ -62,7 +88,7 @@ SocketTCP::State SocketTCP::TCPSend(const char *data, int size) const
 	CONNECT_REQUIRED;
 	int total = 0;
 	int bytesleft = size; 
-	int n = 0;
+    long n = 0;
 	while (total < size) {
 		n = send(sock, data + total, bytesleft, 0);
 		if (n == -1) { throw SocketException{"Error while sending data to the remote host"}; }
@@ -77,7 +103,7 @@ SocketTCP::State SocketTCP::TCPSendString(const std::string& s) const
 	return TCPSend(s.c_str(), static_cast<int>(s.length()));
 }
 
-SocketTCP::State SocketTCP::TCPReceive(void * data, int size, int & received) const
+SocketTCP::State SocketTCP::TCPReceive(void * data, int size, long & received) const
 {
 	CONNECT_REQUIRED;
 	received = recv(sock, static_cast<char*>(data), size, 0);
@@ -102,7 +128,7 @@ SocketTCP::State SocketTCP::TCPReceiveChar(char* c) const
 size_t SocketTCP::read(char* buffer, int size) const
 {
 	CONNECT_REQUIRED;
-	int bytesRead = 0;
+    long bytesRead = 0;
 	if (!isReadyToRead())
 	{
 		throw SocketException{ "Timed out reception" };
@@ -149,6 +175,6 @@ size_t SocketTCP::TCPReceiveUntil(std::string & line, const std::string & end) c
 SocketTCP::~SocketTCP()
 {
 	//no need for wsaclean, os will take care of it
-	closesocket(sock);
+    CLOSE(sock);
 	sock = INVALID_SOCKET;
 }
--- SocketTCP.h
+++ SocketTCP.h
@@ -1,5 +1,10 @@
 #pragma once
+#ifdef _MSC_VER
 #include <WinSock2.h>
+#else
+#define SOCKET int
+#define INVALID_SOCKET -1
+#endif
 #include <string>
 /*this library supports only blocking connections and windows only*/
 
@@ -20,9 +25,10 @@ public:
 	SocketTCP();
 	/*Blocking API*/
 	SocketTCP::State TCPConnect(const std::string& remoteAddress, unsigned short remotePort, int timeout = 5);
+    size_t TCPReveiveUtilClosed(std::string &buffer);
 	SocketTCP::State TCPSend(const char* data, int size) const;
 	SocketTCP::State TCPSendString(const std::string& s) const;
-	SocketTCP::State TCPReceive(void *data, int size, int &received) const;
+    SocketTCP::State TCPReceive(void *data, int size, long &received) const;
 	SocketTCP::State TCPReceiveChar(char *c) const;
 	size_t			 TCPReceiveUntil(std::string& line, const std::string& end = "\r\n") const;
 	/*Non blocking API*/
@@ -32,8 +38,10 @@ public:
 	~SocketTCP();
 private:
 	int timeout = 5;
-	SOCKET sock = INVALID_SOCKET;
+#ifdef _MSC_VER
 	WSADATA wsa {};
+#endif
+    SOCKET sock = INVALID_SOCKET;
 	State state = State::Disconnected;
 	size_t read(char * buffer, int size) const;
 	bool isReadyToRead() const;
